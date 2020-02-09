03f30655b6859934fd1cb70a7236b218cf6ef1ab
Chidubem Ovbiebo <chidubemovbiebo@gmail.com>
2020-02-09 22:37:35 +0100

    Basic firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:vsfluttertest/utils/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:timeago/timeago.dart' as timeago;
 
 void main() => runApp(ReminderApp());
 
@@ -9,6 +12,9 @@ class ReminderApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Reminder",
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
       home: Home(),
     );
   }
@@ -30,69 +36,89 @@ class Home extends StatelessWidget {
           ),
         ),
         actions: <Widget>[
-          Icon(Icons.filter_list),
-          SizedBox(
-            width: SizeConfig.horizontalBlockSize * 3,
+          // Icon(Icons.filter_list),
+          // SizedBox(
+          //   width: SizeConfig.horizontalBlockSize * 3,
+          // ),
+          // Icon(Icons.search),
+          // SizedBox(
+          //   width: SizeConfig.horizontalBlockSize * 3,
+          // ),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Firestore.instance.collection('Reminders').add({
+                "Subject": "Move forward",
+                "Time": Timestamp.fromDate(DateTime.now().add(Duration(minutes: 10))),
+              });
+            },
           ),
-          Icon(Icons.search),
-          SizedBox(
-            width: SizeConfig.horizontalBlockSize * 3,
-          ),
-          Icon(Icons.add),
           SizedBox(
             width: 32,
           ),
         ],
       ),
-      body: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        padding: EdgeInsets.only(top: 16, bottom: 16, left: 32, right: 32),
-        itemCount: 10 * 2,
-        itemBuilder: (context, index) {
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('Reminders')
+            .orderBy("Time", descending: false)
+            .snapshots(),
+        builder: (context, snapshot) {
           SizeConfig().init(context);
-          if (index.isOdd)
-            return Divider(
-              height: SizeConfig.verticalBlockSize * 5,
-            );
-          return Container(
-            padding: EdgeInsets.all(32),
-            height: SizeConfig.verticalBlockSize * 60,
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(42, 42, 42, 1),
-              borderRadius: BorderRadius.all(Radius.circular(32)),
-            ),
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    height: SizeConfig.horizontalBlockSize * 40,
-                    width: SizeConfig.horizontalBlockSize * 40,
-                    decoration: BoxDecoration(
-                        color: Colors.green, shape: BoxShape.circle),
-                  ),
+          if (!snapshot.hasData) return const Text('Fetching data');
+          return ListView.builder(
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.only(top: 16, bottom: 16, left: 32, right: 32),
+            itemCount: snapshot.data.documents.length * 2,
+            itemBuilder: (context, i) {
+              if (i.isOdd)
+                return Divider(
+                  height: SizeConfig.verticalBlockSize * 5,
+                );
+              final index = i ~/ 2;
+              DocumentSnapshot document = snapshot.data.documents[index];
+              DateFormat dateFormat = DateFormat("HH:mm");
+              return Container(
+                padding: EdgeInsets.all(32),
+                height: SizeConfig.verticalBlockSize * 60,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(42, 42, 42, 1),
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
                 ),
-                Text(
-                  "Study",
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: questrialStyle.copyWith(
-                    fontSize: SizeConfig.horizontalBlockSize * 7,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        height: SizeConfig.horizontalBlockSize * 40,
+                        width: SizeConfig.horizontalBlockSize * 40,
+                        decoration: BoxDecoration(
+                            color: Colors.green, shape: BoxShape.circle),
+                      ),
+                    ),
+                    Text(
+                      document['Subject'],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: questrialStyle.copyWith(
+                        fontSize: SizeConfig.horizontalBlockSize * 7,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: SizeConfig.horizontalBlockSize * 2,
+                    ),
+                    Text(
+                      dateFormat.format(document['Time'].toDate()),
+                      style: questrialStyle.copyWith(
+                        fontSize: SizeConfig.horizontalBlockSize * 5,
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: SizeConfig.horizontalBlockSize * 2,
-                ),
-                Text(
-                  "Demilade",
-                  style: questrialStyle.copyWith(
-                    fontSize: SizeConfig.horizontalBlockSize * 5,
-                    color: Color.fromRGBO(255, 255, 255, 0.4),
-                  ),
-                ),
-              ],
-            ),
+              );
+            },
           );
         },
       ),
