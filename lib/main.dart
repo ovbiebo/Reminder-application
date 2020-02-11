d014d9e2d257814c40d3da0435b3b8845d849325
Chidubem Ovbiebo <chidubemovbiebo@gmail.com>
2020-02-11 14:09:56 +0100

    Updated for firestore delete

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,9 @@ class Home extends StatelessWidget {
             onPressed: () {
               Firestore.instance.collection('Reminders').add({
                 "Subject": "Move forward",
-                "Time": Timestamp.fromDate(DateTime.now().add(Duration(minutes: 10))),
+                "Time": Timestamp.fromDate(
+                    DateTime.now().add(Duration(minutes: 10))),
+                "From": Timestamp.fromDate(DateTime.now()),
               });
             },
           ),
@@ -58,70 +60,127 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('Reminders')
-            .orderBy("Time", descending: false)
-            .snapshots(),
-        builder: (context, snapshot) {
-          SizeConfig().init(context);
-          if (!snapshot.hasData) return const Text('Fetching data');
-          return ListView.builder(
-            physics: BouncingScrollPhysics(),
-            padding: EdgeInsets.only(top: 16, bottom: 16, left: 32, right: 32),
-            itemCount: snapshot.data.documents.length * 2,
-            itemBuilder: (context, i) {
-              if (i.isOdd)
-                return Divider(
-                  height: SizeConfig.verticalBlockSize * 5,
-                );
-              final index = i ~/ 2;
-              DocumentSnapshot document = snapshot.data.documents[index];
-              DateFormat dateFormat = DateFormat("HH:mm");
-              return Container(
-                padding: EdgeInsets.all(32),
-                height: SizeConfig.verticalBlockSize * 60,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(42, 42, 42, 1),
-                  borderRadius: BorderRadius.all(Radius.circular(32)),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        height: SizeConfig.horizontalBlockSize * 40,
-                        width: SizeConfig.horizontalBlockSize * 40,
-                        decoration: BoxDecoration(
-                            color: Colors.green, shape: BoxShape.circle),
-                      ),
-                    ),
-                    Text(
-                      document['Subject'],
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: questrialStyle.copyWith(
-                        fontSize: SizeConfig.horizontalBlockSize * 7,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(
-                      height: SizeConfig.horizontalBlockSize * 2,
-                    ),
-                    Text(
-                      dateFormat.format(document['Time'].toDate()),
-                      style: questrialStyle.copyWith(
-                        fontSize: SizeConfig.horizontalBlockSize * 5,
-                        color: Color.fromRGBO(255, 255, 255, 0.4),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-      ),
+      body: RemindersList(),
     );
   }
+}
+
+class RemindersList extends StatefulWidget {
+  @override
+  RemindersListState createState() {
+    return RemindersListState();
+  }
+}
+
+class RemindersListState extends State<RemindersList> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance
+          .collection('Reminders')
+          .orderBy("Time", descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      physics: BouncingScrollPhysics(),
+      padding: EdgeInsets.only(top: 16, bottom: 16, left: 32, right: 32),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+    DateFormat dateFormat = DateFormat("HH:mm");
+
+    return Dismissible(
+      key: Key(record.time.toString()),
+      child: Container(
+        padding: EdgeInsets.all(32),
+        margin: EdgeInsets.only(bottom: 32),
+        height: SizeConfig.verticalBlockSize * 60,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(42, 42, 42, 1),
+          borderRadius: BorderRadius.all(Radius.circular(32)),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: SizeConfig.horizontalBlockSize * 40,
+                width: SizeConfig.horizontalBlockSize * 40,
+                decoration:
+                    BoxDecoration(color: Colors.green, shape: BoxShape.circle),
+              ),
+            ),
+            Text(
+              record.subject,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: questrialStyle.copyWith(
+                fontSize: SizeConfig.horizontalBlockSize * 7,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.horizontalBlockSize * 2,
+            ),
+            Text(
+              dateFormat.format(record.time.toDate()),
+              style: questrialStyle.copyWith(
+                fontSize: SizeConfig.horizontalBlockSize * 5,
+                color: Color.fromRGBO(255, 255, 255, 0.4),
+              ),
+            ),
+          ],
+        ),
+      ),
+      onDismissed: (direction) {
+        // Firestore.instance.collection('Reminders').document(record.reference.documentID).delete();
+        // setState(() {
+        Firestore.instance.runTransaction((Transaction delTransaction) async {
+          await delTransaction.delete(Firestore.instance
+              .collection('Reminders')
+              .document(record.reference.documentID));
+        });
+        // snapshot.removeAt(index);
+        // });
+        // setState(() {
+        //   Firestore.instance.runTransaction((Transaction delTransaction) async {
+        //     await delTransaction.delete(Firestore.instance
+        //         .collection('Reminders')
+        //         .document(record.reference.documentID));
+        //   });
+        //   // snapshot.removeAt(index);
+        // });
+        Scaffold.of(context)
+            .showSnackBar(SnackBar(content: Text("Reminder deleted.")));
+      },
+    );
+  }
+}
+
+class Record {
+  final String subject;
+  final Timestamp time;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['Subject'] != null),
+        assert(map['Time'] != null),
+        subject = map['Subject'],
+        time = map['Time'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$subject:$time>";
 }
